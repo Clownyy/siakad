7580786137f76f8e02cec1739bdd7063269c28ab
Aldi04 <jumialdi60@gmail.com>
2019-11-01 08:35:29 +0700

    TEMPLATE ADMIN

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,345 @@
+<!DOCTYPE html>
+<html>
+
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+    <meta name="author" content="Creative Tim">
+    <title>SIAKAD</title>
+    <!-- Extra details for Live View on GitHub Pages -->
+    <!-- Canonical SEO -->
+    <link rel="canonical" href="https://www.creative-tim.com/product/argon-dashboard-pro" />
+    <!--  Social tags      -->
+    <meta name="keywords" content="dashboard, bootstrap 4 dashboard, bootstrap 4 design, bootstrap 4 system, bootstrap 4, bootstrap 4 uit kit, bootstrap 4 kit, argon, argon ui kit, creative tim, html kit, html css template, web template, bootstrap, bootstrap 4, css3 template, frontend, responsive bootstrap template, bootstrap ui kit, responsive ui kit, argon dashboard">
+    <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+    <!-- Schema.org markup for Google+ -->
+    <meta itemprop="name" content="Argon - Premium Dashboard for Bootstrap 4 by Creative Tim">
+    <meta itemprop="description" content="Start your development with a Dashboard for Bootstrap 4.">
+    <meta itemprop="image" content="../../../../s3.amazonaws.com/creativetim_bucket/products/137/original/opt_adp_thumbnail.jpg">
+    <!-- Twitter Card data -->
+    <meta name="twitter:card" content="product">
+    <meta name="twitter:site" content="@creativetim">
+    <meta name="twitter:title" content="Argon - Premium Dashboard for Bootstrap 4 by Creative Tim">
+    <meta name="twitter:description" content="Start your development with a Dashboard for Bootstrap 4.">
+    <meta name="twitter:creator" content="@creativetim">
+    <meta name="twitter:image" content="../../../../s3.amazonaws.com/creativetim_bucket/products/137/original/opt_adp_thumbnail.jpg">
+    <!-- Open Graph data -->
+    <meta property="fb:app_id" content="655968634437471">
+    <meta property="og:title" content="Argon - Premium Dashboard for Bootstrap 4 by Creative Tim" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="https://demos.creative-tim.com/argon-dashboard/index.html" />
+    <meta property="og:image" content="../../../../s3.amazonaws.com/creativetim_bucket/products/137/original/opt_adp_thumbnail.jpg" />
+    <meta property="og:description" content="Start your development with a Dashboard for Bootstrap 4." />
+    <meta property="og:site_name" content="Creative Tim" />
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('/assets/img/brand/favicon.png')}}" type="image/png">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+    <!-- Icons -->
+    <link rel="stylesheet" href="{{asset ('/assets/vendor/nucleo/css/nucleo.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('/assets/vendor/%40fortawesome/fontawesome-free/css/all.min.css')}}" type="text/css">
+    <!-- Page plugins -->
+    <!-- Argon CSS -->
+    <link rel="stylesheet" href="{{ asset('/assets/css/argon.mine209.css?v=1.0.0')}}" type="text/css">
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                '../../../../www.googletagmanager.com/gtm5445.html?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-NKDMSK6');
+    </script>
+    <!-- End Google Tag Manager -->
+</head>
+
+<body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKDMSK6" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <!-- Sidenav -->
+    <nav class="sidenav navbar navbar-vertical fixed-left navbar-expand-xs navbar-light bg-white" id="sidenav-main">
+        <div class="scrollbar-inner">
+            <!-- Brand -->
+            <div class="sidenav-header d-flex align-items-center">
+                <a class="navbar-brand" href="#">
+                    <img src="{{ asset('/assets/img/brand/blue.png')}}" class="navbar-brand-img" alt="...">
+                </a>
+                <div class="ml-auto">
+                    <!-- Sidenav toggler -->
+                    <div class="sidenav-toggler d-none d-xl-block" data-action="sidenav-unpin" data-target="#sidenav-main">
+                        <div class="sidenav-toggler-inner">
+                            <i class="sidenav-toggler-line"></i>
+                            <i class="sidenav-toggler-line"></i>
+                            <i class="sidenav-toggler-line"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="navbar-inner">
+                <!-- Collapse -->
+                <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+                    <!-- Nav items -->
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#navbar-dashboards" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-dashboards">
+                                <i class="ni ni-shop text-primary"></i>
+                                <span class="nav-link-text">Dashboards</span>
+                            </a>
+                            <div class="collapse show" id="navbar-dashboards">
+                                <ul class="nav nav-sm flex-column">
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Dashboard</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Alternative</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#navbar-examples" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-examples">
+                                <i class="ni ni-ungroup text-orange"></i>
+                                <span class="nav-link-text">Examples</span>
+                            </a>
+                            <div class="collapse" id="navbar-examples">
+                                <ul class="nav nav-sm flex-column">
+                                    <li class="nav-item">
+                                        <a href="#t" class="nav-link">Pricing</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Login</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Register</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Lock</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Timeline</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Profile</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#navbar-components" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-components">
+                                <i class="ni ni-ui-04 text-info"></i>
+                                <span class="nav-link-text">Components</span>
+                            </a>
+                            <div class="collapse" id="navbar-components">
+                                <ul class="nav nav-sm flex-column">
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Buttons</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Cards</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Grid</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Notifications</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Icons</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Typography</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#navbar-multilevel" class="nav-link" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-multilevel">Multi level</a>
+                                        <div class="collapse show" id="navbar-multilevel" style="">
+                                            <ul class="nav nav-sm flex-column">
+                                                <li class="nav-item">
+                                                    <a href="#" class="nav-link ">Third level menu</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a href="#" class="nav-link ">Just another link</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a href="#" class="nav-link ">One last link</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#navbar-forms" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-forms">
+                                <i class="ni ni-single-copy-04 text-pink"></i>
+                                <span class="nav-link-text">Forms</span>
+                            </a>
+                            <div class="collapse" id="navbar-forms">
+                                <ul class="nav nav-sm flex-column">
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Elements</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Components</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Validation</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#navbar-tables" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-tables">
+                                <i class="ni ni-align-left-2 text-default"></i>
+                                <span class="nav-link-text">Tables</span>
+                            </a>
+                            <div class="collapse" id="navbar-tables">
+                                <ul class="nav nav-sm flex-column">
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Tables</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Sortable</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Datatables</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#navbar-maps" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-maps">
+                                <i class="ni ni-map-big text-primary"></i>
+                                <span class="nav-link-text">Maps</span>
+                            </a>
+                            <div class="collapse" id="navbar-maps">
+                                <ul class="nav nav-sm flex-column">
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Google</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">Vector</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="ni ni-archive-2 text-green"></i>
+                                <span class="nav-link-text">Widgets</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="ni ni-chart-pie-35 text-info"></i>
+                                <span class="nav-link-text">Charts</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="ni ni-calendar-grid-58 text-red"></i>
+                                <span class="nav-link-text">Calendar</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- Divider -->
+                    <hr class="my-3">
+                    <!-- Heading -->
+                    <h6 class="navbar-heading p-0 text-muted">Documentation</h6>
+                    <!-- Navigation -->
+                    <ul class="navbar-nav mb-md-3">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/docs/getting-started/overview.html" target="_blank">
+                                <i class="ni ni-spaceship"></i>
+                                <span class="nav-link-text">Getting started</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/docs/foundation/colors.html" target="_blank">
+                                <i class="ni ni-palette"></i>
+                                <span class="nav-link-text">Foundation</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/docs/components/alerts.html" target="_blank">
+                                <i class="ni ni-ui-04"></i>
+                                <span class="nav-link-text">Components</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/docs/plugins/charts.html" target="_blank">
+                                <i class="ni ni-chart-pie-35"></i>
+                                <span class="nav-link-text">Plugins</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    @yield('content')
+
+    <!-- Argon Scripts -->
+    <!-- Core -->
+    <script src="{{ asset('/assets/vendor/jquery/dist/jquery.min.js')}}"></script>
+    <script src="{{ asset('/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ asset('/assets/vendor/js-cookie/js.cookie.js')}}"></script>
+    <script src="{{ asset('/assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js')}}"></script>
+    <script src="{{ asset('/assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js')}}"></script>
+    <script src="{{ asset('/assets/vendor/lavalamp/js/jquery.lavalamp.min.js')}}"></script>
+    <!-- Optional JS -->
+    <script src="{{ asset('/assets/vendor/chart.js/dist/Chart.min.js')}}"></script>
+    <script src="{{ asset('/assets/vendor/chart.js/dist/Chart.extension.js')}}"></script>
+    <!-- Argon JS -->
+    <script src="{{ asset('/assets/js/argon.mine209.js?v=1.0.0')}}"></script>
+    <!-- Demo JS - remove this in your project -->
+    <script src="{{ asset('/assets/js/demo.min.js')}}"></script>
+    <script>
+        // Facebook Pixel Code Don't Delete
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window,
+            document, 'script', '../../../../connect.facebook.net/en_US/fbevents.js');
+
+        try {
+            fbq('init', '111649226022273');
+            fbq('track', "PageView");
+
+        } catch (err) {
+            console.log('Facebook Track Error:', err);
+        }
+    </script>
+    <noscript>
+        <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=111649226022273&amp;ev=PageView&amp;noscript=1" />
+    </noscript>
+</body>
+
+</html>
+@yield('foot-content')
